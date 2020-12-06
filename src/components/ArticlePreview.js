6bff1999a65d371b35e9dea3f599bb75016889f1
ross-u <cirkovic.uros.srb@gmail.com>
2020-12-06 23:14:55 +0100

    Update ArticlePreview

--- src/components/ArticlePreview.js
+++ src/components/ArticlePreview.js
@@ -4,7 +4,8 @@ import React, { Component } from 'react';
 class ArticlePreview extends Component {
   state = {
     title: undefined,
-    description: undefined
+    description: undefined,
+    
   }
 
   componentDidMount() {
@@ -13,13 +14,24 @@ class ArticlePreview extends Component {
   }
 
 
+  loadMore = () => {
+    this.setState({ content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, nihil iste itaque eum fuga autem neque nemo qui inventore voluptas! Delectus dicta aliquam sint a! Aut voluptates laboriosam vitae repellendus." })
+  }
+
+
   render() {
     return (
-      <div className="article-preview" style={{ background: this.props.color }}>
+      <div className="article-preview" style={{ background: '#CDDFFF' }}>
         <h3>{this.state.title ? this.state.title : null}</h3>
         <p>{this.state.description ? this.state.description : null}</p>
 
-        <div className="fade"></div>
+          {
+            this.state.content
+              ? <p>{this.state.content}</p>
+              : <div className="fade">
+                  <button className="btn-more" onClick={this.loadMore}>More</button>
+                </div>
+          }
       </div>
     )
   }
