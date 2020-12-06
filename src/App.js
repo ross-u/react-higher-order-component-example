3c39651007c5b902a1207a33c15d16e781b959ee
ross-u <cirkovic.uros.srb@gmail.com>
2020-12-06 22:59:03 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,19 +4,23 @@ import ArticlePreview from './components/ArticlePreview';
 import Navbar from './components/Navbar';
 
 
-const articles = [
-  { title: 'Help wanted: Autonomous robot guide', description: 'While we are currently embroiled in personal and professional circumstances unimaginable even three months ago, these very challenges will form the basis of incredibly innovative ideas.' },
-  { title: 'Building and investing in the ‘human needs economy', description: 'Products and services that have material impact on basic needs and livelihoods and address a coredraw on a consumer’s time, money or energy. For 2020, we are focusing ...' },
-  { title: 'Facebook to acquire Giphy in a deal worth $400 million', description: 'Facebook  will acquire Giphy, the web-based animated gif search engine and platform provider, Facebook confirmed today, in a deal worth around $400 million, according to a report.' },
-]
+class App extends React.Component {
+  state = {
+    articles: [
+      { title: 'Help wanted: Autonomous robot guide', description: 'While we are currently embroiled in personal and professional circumstances unimaginable even three months ago, these very challenges will form the basis of incredibly innovative ideas.' },
+      { title: 'Building and investing in the ‘human needs economy', description: 'Products and services that have material impact on basic needs and livelihoods and address a coredraw on a consumer’s time, money or energy. For 2020, we are focusing ...' },
+      { title: 'Facebook to acquire Giphy in a deal worth $400 million', description: 'Facebook  will acquire Giphy, the web-based animated gif search engine and platform provider, Facebook confirmed today, in a deal worth around $400 million, according to a report.' },
+    ]
+  }
+  render() {
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      {articles.map((article) => <ArticlePreview {...article} />)}
-    </div>
-  );
+    return (
+      <div className="App">
+        <Navbar />
+        {this.state.articles.map((article) => <ArticlePreview {...article} />)}
+      </div>
+    );
+  }
 }
 
 export default App;
